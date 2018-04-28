8bd6637ebc3d97f02699c4106f6924f4fd579584
Fergus Ruston <fergus.ruston@gmail.com>
2018-04-28 12:20:07 +0100

    better dropdown component

--- app/components/dropdown.js
+++ app/components/dropdown.js
@@ -4,6 +4,10 @@ var templater = require("../templater"),
 var button = templater.button,
 	div = templater.div,
 	h1 = templater.h1,
+	h3 = templater.h3,
+	pre = templater.pre,
+	code = templater.code,
+	span = templater.span,
 	paragraph = templater.p,
 	fragment = templater.fragment;
 
@@ -12,6 +16,16 @@ function Dropdown() {
 }
 
 Dropdown.prototype.intro = function() {
+	return fragment([
+		h1({ class: "margin-bottom padding-bottom border-bottom border-color-grey", text: "Dropdown" }),
+		paragraph({
+			class: "margin-vertical line-height-1p5",
+			text: "A versatile dropdown component."
+		})
+	]);
+};
+
+Dropdown.prototype.onHover = function() {
 	var trigger = button({
 		class: "button box-shadow",
 		text: "Hover to show dropdown"
@@ -20,6 +34,7 @@ Dropdown.prototype.intro = function() {
 	var dropdown = new DropdownComponent({
 		trigger: trigger,
 		triggeredOnHover: true,
+		alignLeft: true,
 		content: function() {
 			return div({
 				class: "padding-all margin-top-small background-color-white border-radius-all border-all border-color-grey inline-block box-shadow",
@@ -30,22 +45,111 @@ Dropdown.prototype.intro = function() {
 		}
 	});
 
-	return fragment([
-		h1({ class: "margin-bottom padding-bottom border-bottom border-color-grey", text: "Dropdown" }),
-		paragraph({
-			class: "margin-vertical line-height-1p5",
-			text: "A dropdown component."
-		}),
-		dropdown.element
-	]);
+
+	return div({
+		class: "margin-vertical",
+		children: [
+			h3({ class: "margin-bottom padding-bottom border-bottom border-color-grey", text: "Dropdown On Hover" }),
+			dropdown.element,
+			pre({
+				class: "preformatted margin-vertical-medium padding-all-small",
+				content: code({ class: "code", text: ".dropdown__container.dropdown__container--shown-on-hover .dropdown" })
+			}),
+			paragraph({
+				class: "margin-vertical-medium",
+				children: [
+					span({
+						text: "Hovering over any element contained within the "
+					}),
+					code({
+						text: ".dropdown__container"
+					}),
+					span({
+						text: " will be considered a trigger."
+					})
+				]
+			})
+		]
+	});
+};
+
+Dropdown.prototype.onHoverCentered = function() {
+	var trigger = button({
+		class: "button box-shadow",
+		text: "Hover to show dropdown"
+	});
+
+	var dropdown = new DropdownComponent({
+		trigger: trigger,
+		triggeredOnHover: true,
+		alignCenter: true,
+		content: function() {
+			return div({
+				class: "padding-all margin-top-small background-color-white border-radius-all border-all border-color-grey inline-block box-shadow",
+				content: paragraph({
+					text: "Dropdown Content"
+				})
+			})
+		}
+	});
+
+
+	return div({
+		class: "margin-vertical",
+		children: [
+			h3({ class: "margin-bottom padding-bottom border-bottom border-color-grey", text: "Centered dropdown" }),
+			dropdown.element,
+			pre({
+				class: "preformatted margin-vertical-medium padding-all-small",
+				content: code({ class: "code", text: ".dropdown__container.dropdown__container--align-center .dropdown" })
+			})
+		]
+	});
+};
+
+Dropdown.prototype.onHoverAlignRight = function() {
+	var trigger = button({
+		class: "button box-shadow",
+		text: "Hover to show dropdown"
+	});
+
+	var dropdown = new DropdownComponent({
+		trigger: trigger,
+		triggeredOnHover: true,
+		alignRight: true,
+		content: function() {
+			return div({
+				class: "padding-all margin-top-small background-color-white border-radius-all border-all border-color-grey inline-block box-shadow",
+				content: paragraph({
+					text: "Dropdown Content"
+				})
+			})
+		}
+	});
+
+
+	return div({
+		class: "margin-vertical",
+		children: [
+			h3({ class: "margin-bottom padding-bottom border-bottom border-color-grey", text: "Aligned right dropdown" }),
+			dropdown.element,
+			pre({
+				class: "preformatted margin-vertical-medium padding-all-small",
+				content: code({ class: "code", text: ".dropdown__container.dropdown__container--align-right .dropdown" })
+			})
+		]
+	});
 };
 
 Dropdown.prototype.render = function() {
-	var intro = this.intro();
+	var intro = this.intro(),
+		onHover = this.onHover(),
+		onHoverCentered = this.onHoverCentered(),
+		onHoverAlignRight = this.onHoverAlignRight();
 
 	return div({
 		class: "max-width-5 centred padding-all-2 margin-vertical-2 border background-color-white border-all border-color-grey box-shadow border-radius-all",
-		children: [intro]
+		children: [intro,onHover,onHoverCentered,onHoverAlignRight]
 	});
 };
 
--- components/dropdown/index.js
+++ components/dropdown/index.js
@@ -6,6 +6,10 @@ function Dropdown(props) {
 	props = props || {};
 	this.trigger = props.trigger;
 	this.triggeredOnHover = props.triggeredOnHover;
+	this.triggeredOnClick = props.triggeredOnClick;
+	this.alignLeft = props.alignLeft;
+	this.alignRight = props.alignRight;
+	this.alignCenter = props.alignCenter;
 	this.content = props.content;
 	this.element = this.render();
 	this.addEventListeners();
@@ -13,13 +17,44 @@ function Dropdown(props) {
 
 Dropdown.prototype.addEventListeners = function() {
 	var self = this;
+	if(this.triggeredOnClick) {
+		this.trigger.addEventListener("click",function(event) {
+			event.preventDefault();
+
+			self.open();
+
+			document.addEventListener("click",function(event) {
+				if(!self.element.contains(event.target)) self.close();
+			});
+		});
+	}
+};
+
+Dropdown.prototype.open = function() {
+	this.element.classList.add("dropdown__container--open");
+};
+
+Dropdown.prototype.close = function() {
+	this.element.classList.remove("dropdown__container--open");
 };
 
 Dropdown.prototype.render = function() {
 	var className = "dropdown__container";
 
 	if(this.triggeredOnHover) {
-		className += " dropdown__container--on-hover";
+		className += " dropdown__container--shown-on-hover";
+	}
+
+	if(this.triggeredOnClick) {
+		className += " dropdown__container--shown-on-click";
+	}
+
+	if(this.alignLeft) {
+		className += " dropdown__container--align-left";
+	} else if(this.alignRight) {
+		className += " dropdown__container--align-right";
+	} else if(this.alignCenter) {
+		className += " dropdown__container--align-center";
 	}
 
 	return div({
