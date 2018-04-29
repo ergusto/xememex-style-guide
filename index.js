9975ed5c54207d3c5f2e8aaf2b209671880d8adf
Fergus Ruston <fergus.ruston@gmail.com>
2018-04-29 01:56:23 +0100

    add no-touch class to document on non-touch devices so we can only apply :hover effects on non-touch devices

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var app = require("./app");
 
 document.addEventListener("DOMContentLoaded", function() {
+	if (!('ontouchstart' in document.documentElement)) {
+		document.documentElement.classList.add('no-touch');
+	}
+
 	var appNode = document.querySelector("#app");
 	app(appNode);
 });
